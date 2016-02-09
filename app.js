f9aea0c5241934bb606c06dc7978b3b3777630e6
shuchi <shuchi@b5m.com>
2016-02-09 11:28:07 +0800

    'format'

--- app.js
+++ app.js
@@ -16,22 +16,22 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
 app.use(function (req, res) {
-    res.sendfile('./public/index.html');
+  res.sendfile('./public/index.html');
 });
 
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -39,27 +39,27 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
-var server = app.listen(port, function() {
-    console.log('Express server listening on port ' + server.address().port);
+var server = app.listen(port, function () {
+  console.log('Express server listening on port ' + server.address().port);
 });
 
 chatServer.listen(server);
--- models/user.js
+++ models/user.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var User = new Schema({
-    email: String,
-    name: String,
-    avatarUrl: String
+  email: String,
+  name: String,
+  avatarUrl: String
 });
 
 module.exports = User;
